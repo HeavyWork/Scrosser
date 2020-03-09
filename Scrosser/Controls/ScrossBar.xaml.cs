a3eac1bdc1cbd658c7920c10fca8584adddc5174
Il Harper <afanyiyu@hotmail.com>
2020-03-09 09:10:27 +0800

    feat: Add ScrossBar Control

---
+++ Scrosser/Controls/ScrossBar.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Scrosser.Models;
+
+namespace Scrosser.Controls
+{
+    /// <summary>
+    /// ScrossBar.xaml 的交互逻辑
+    /// </summary>
+    public partial class ScrossBar : UserControl
+    {
+        public ScrossBar()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+
+        public static readonly DependencyProperty ScrossProperty = DependencyProperty.Register(
+            "Scross", typeof(Scross), typeof(ScrossBar), new PropertyMetadata(new Scross()));
+
+        public Scross Scross
+        {
+            get => (Scross) GetValue(ScrossProperty);
+            set => SetValue(ScrossProperty, value);
+        }
+
+        public static readonly DependencyProperty OrientationProperty = DependencyProperty.Register(
+            "Orientation", typeof(Orientation), typeof(ScrossBar), new PropertyMetadata(Orientation.Vertical));
+
+        public Orientation Orientation
+        {
+            get => (Orientation) GetValue(OrientationProperty);
+            set => SetValue(OrientationProperty, value);
+        }
+
+    }
+}
