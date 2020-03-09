1db7d5096144ccd3f43804aef4d5ed0b7fa31bb0
Il Harper <afanyiyu@hotmail.com>
2020-03-09 08:02:11 +0800

    feat: Add Scross Class

---
+++ Scrosser/Models/Scross.cs
@@ -0,0 +1,120 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Scrosser.Models
+{
+
+    /// <summary>
+    /// Scross class.
+    /// </summary>
+    public class Scross : INotifyPropertyChanged
+    {
+
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        #region Properties
+
+        private double _horizontalZoom = 1;
+
+        /// <summary>
+        /// Horizontal zoom.
+        /// </summary>
+        public double HorizontalZoom
+        {
+            get => _horizontalZoom;
+            set
+            {
+                _horizontalZoom = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double _verticalZoom = 1;
+
+        /// <summary>
+        /// Vertical zoom.
+        /// </summary>
+        public double VerticalZoom
+        {
+            get => _verticalZoom;
+            set
+            {
+                _verticalZoom = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double _horizontalScroll;
+
+        /// <summary>
+        /// Horizontal scroll.
+        /// </summary>
+        public double HorizontalScroll
+        {
+            get => _horizontalScroll;
+            set
+            {
+                _horizontalScroll = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double _verticalScroll;
+
+        /// <summary>
+        /// Vertical scroll.
+        /// </summary>
+        public double VerticalScroll
+        {
+            get => _verticalScroll;
+            set
+            {
+                _verticalScroll = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double _position;
+
+        /// <summary>
+        /// The position.
+        /// </summary>
+        public double Position
+        {
+            get => _position;
+            set
+            {
+                _position = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double _total;
+
+        /// <summary>
+        /// The total length.
+        /// </summary>
+        public double Total
+        {
+            get => _total;
+            set
+            {
+                _total = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+    }
+
+}
