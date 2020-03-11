76251b27180b5c9c9b4367f6cf3c017f8f1a2bb2
Il Harper <afanyiyu@hotmail.com>
2020-03-11 12:44:01 +0800

    feat: Add Posit Class

---
+++ Scrosser/Models/Posit.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrosser.Models
+{
+
+    /// <summary>
+    /// Position definition.
+    /// </summary>
+    public class Posit : INotifyPropertyChanged
+    {
+
+        public Posit(
+            long total = 0,
+            long position = 0)
+        {
+            _position = position;
+            _total = total;
+        }
+
+        #region DataContext
+
+        /// <summary>
+        /// Start position. Always 0.
+        /// Define a new class inherit from Posit if you want to redefine start position.
+        /// </summary>
+        public double Start => 0;
+
+        private long _position;
+
+        public long Position
+        {
+            get => _position;
+            set
+            {
+                _position = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private long _total;
+
+        public long Total
+        {
+            get => _total;
+            set
+            {
+                _total = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+    }
+
+}
